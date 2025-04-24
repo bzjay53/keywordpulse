1bbb0a4080d167ca896aec466b965a45765dbf88
bzjay53 <bajay53@gmail.com>
2025-04-24 17:32:49 +0900

    Fix: 컴포넌트의 hooks 경로 수정으로 빌드 오류 해결

--- components/KeywordTable.tsx
+++ components/KeywordTable.tsx
@@ -1,8 +1,8 @@
 'use client';
 
 import React, { useState } from 'react';
-import useKeywordScoreBadge from '../hooks/useKeywordScoreBadge';
-import useKeywordSorting from '../hooks/useKeywordSorting';
+import useKeywordScoreBadge from '../app/hooks/useKeywordScoreBadge';
+import useKeywordSorting from '../app/hooks/useKeywordSorting';
 
 interface KeywordInfo {
   keyword: string;
